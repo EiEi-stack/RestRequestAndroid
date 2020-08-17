7b3e55dc4f1ae4bf04caa1104ddba3d91ac012e5
EiEi-stack <yccvenusstudent@gmail.com>
2020-08-17 21:21:35 +0900

    現在住所をGeoCoderを使って表示

--- app/src/main/java/com/example/restrequestandroid/MainActivity.kt
+++ app/src/main/java/com/example/restrequestandroid/MainActivity.kt
@@ -1,5 +1,8 @@
 package com.example.restrequestandroid
 
+import android.app.Activity
+import android.content.Intent
+import android.content.IntentSender
 import android.content.pm.PackageManager
 import android.graphics.BitmapFactory
 import android.location.Address
@@ -7,10 +10,13 @@ import android.location.Geocoder
 import android.location.Location
 import android.os.Bundle
 import android.util.Log
+import android.widget.Button
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
-import com.google.android.gms.location.FusedLocationProviderClient
-import com.google.android.gms.location.LocationServices
+import com.google.android.gms.common.api.ResolvableApiException
+import com.google.android.gms.location.*
+import com.google.android.gms.location.places.ui.PlacePicker
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
@@ -25,13 +31,37 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnMarker
     private lateinit var map: GoogleMap
     private lateinit var fusedLocationClient: FusedLocationProviderClient
     private lateinit var lastLocation: Location
+    private lateinit var locationCallback: LocationCallback
+    private lateinit var locationRequest: LocationRequest
+    private var locationUpdateState = false
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val mapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
-        mapFragment.getMapAsync(this)
+        val showCurrent = findViewById<Button>(R.id.showCurrentPlace)
+        val tv_current = findViewById<TextView>(R.id.tv_currentPlace)
 
+        mapFragment.getMapAsync(this)
         fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+        locationCallback = object : LocationCallback() {
+            override fun onLocationResult(p0: LocationResult) {
+                super.onLocationResult(p0)
+
+                lastLocation = p0.lastLocation
+                placeMarkerOnMap(LatLng(lastLocation.latitude, lastLocation.longitude))
+            }
+        }
+        showCurrent.setOnClickListener() {
+            if (lastLocation != null) {
+                val latestPlace = lastLocation
+                val latlng = LatLng(latestPlace.latitude, latestPlace.latitude)
+                val lastAddress = getAddress(latlng)
+
+                tv_current.text = lastAddress.toString()
+            }
+
+        }
+        createLocationRequest()
     }
 
     override fun onMapReady(googleMap: GoogleMap) {
@@ -72,16 +102,18 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnMarker
 
     private fun placeMarkerOnMap(location: LatLng) {
         val markerOptions = MarkerOptions().position(location)
+        val titleStr = getAddress(location)
+        markerOptions.title(titleStr)
+
         markerOptions.icon(
             BitmapDescriptorFactory.fromBitmap(
                 BitmapFactory.decodeResource(
                     resources,
-                    R.mipmap.ic_user_location
+                    R.mipmap.waling
                 )
             )
         )
-        val titleStr = getAddress(location)
-        markerOptions.title(titleStr)
+
         map.addMarker(markerOptions)
     }
 
@@ -91,21 +123,112 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnMarker
         val addresses: List<Address>?
         val address: Address?
         var addressText = ""
+        var index = 0;
+        var strbDr = StringBuilder("")
         try {
             addresses = geocoder.getFromLocation(latLng.latitude, latLng.longitude, 1)
             if (null != addresses && !addresses.isEmpty()) {
                 address = addresses[0]
-                for (i in 0 until address.maxAddressLineIndex) {
-                    addressText += if (i == 0) address.getAddressLine(i) else "/n" + address.getAddressLine(
-                        i
-                    )
-
-                }
+                do {
+//                    addressText += address.getAddressLine(0)
+                    strbDr.append(address.getAddressLine(index)).append("\n")
+                    index++
+                } while (index<address.maxAddressLineIndex)
+                addressText=strbDr.toString()
             }
         } catch (e: IOException) {
             Log.e("MapsActivity", e.localizedMessage)
         }
+
         return addressText
+    }
+
+    private fun startLocationUpdates() {
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                android.Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION),
+                LOCATION_PERMISSION_REQUEST_CODE
+            )
+            return
+        }
+        fusedLocationClient.requestLocationUpdates(locationRequest, locationCallback, null)
+    }
+
+    private fun createLocationRequest() {
+        locationRequest = LocationRequest()
+        locationRequest.interval = 10000
+        locationRequest.fastestInterval = 5000
+        locationRequest.priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+
+        val builder = LocationSettingsRequest.Builder()
+            .addLocationRequest(locationRequest)
+
+        val client = LocationServices.getSettingsClient(this)
+        val task = client.checkLocationSettings(builder.build())
+
+        task.addOnSuccessListener {
+            locationUpdateState = true
+            startLocationUpdates()
+        }
+        task.addOnFailureListener { e ->
+            if (e is ResolvableApiException) {
+                try {
+                    e.startResolutionForResult(
+                        this@MainActivity,
+                        REQUEST_CHECK_SETTINGS
+                    )
+                } catch (sendEx: IntentSender.SendIntentException) {
+                }
+            }
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == REQUEST_CHECK_SETTINGS) {
+            if (resultCode == Activity.RESULT_OK) {
+                locationUpdateState = true
+                startLocationUpdates()
+            }
+        }
+        if (requestCode == PLACE_PICKER_REQUEST) {
+            if (resultCode == RESULT_OK) {
+                val place = PlacePicker.getPlace(this, data)
+                var addressText = place.name.toString()
+                addressText += "\n" + place.address.toString()
+
+                placeMarkerOnMap(place.latLng)
+            }
+        }
+    }
+
+    override fun onPause() {
+        super.onPause()
+        fusedLocationClient.removeLocationUpdates(locationCallback)
+    }
+
+    public override fun onResume() {
+        super.onResume()
+        if (!locationUpdateState) {
+            startLocationUpdates()
+        }
+    }
+
+    private fun loadPlacePicker() {
+//        val builder = PlacePicker.IntentBuilder()
+//
+//        try {
+//            startActivityForResult(builder.build(this@MainActivity), PLACE_PICKER_REQUEST)
+//        } catch (e: GooglePlayServicesRepairableException) {
+//            e.printStackTrace()
+//        } catch (e: GooglePlayServicesNotAvailableException) {
+//            e.printStackTrace()
+//        }
     }
 
     override fun onMarkerClick(p0: Marker?): Boolean {
@@ -114,6 +237,8 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnMarker
 
     companion object {
         private const val LOCATION_PERMISSION_REQUEST_CODE = 1
+        private const val REQUEST_CHECK_SETTINGS = 2
+        private const val PLACE_PICKER_REQUEST = 3
     }
 
 }

c89d31447f51c46c92ed46fbd025fe75c50a1091
EiEi-stack <yccvenusstudent@gmail.com>
2020-08-24 21:38:32 +0900

    勤務表App作成 各月の日付を準備 設計

--- app/src/main/java/com/example/restrequestandroid/HomeActivity.kt
+++ app/src/main/java/com/example/restrequestandroid/HomeActivity.kt
@@ -1,19 +1,28 @@
 package com.example.restrequestandroid
 
-    import android.content.Intent
-    import androidx.appcompat.app.AppCompatActivity
-    import android.os.Bundle
-    import android.widget.Button
+import android.os.Bundle
+import android.widget.CalendarView
+import androidx.appcompat.app.AppCompatActivity
+import java.text.SimpleDateFormat
+import java.util.*
 
-    class HomeActivity : AppCompatActivity() {
+class HomeActivity : AppCompatActivity() {
+    lateinit var calendarView: CalendarView
+    lateinit var format: SimpleDateFormat
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+        val simpleDateFormat = SimpleDateFormat("yyyy/MM/dd")
+        val dayFormat = SimpleDateFormat("EEE", Locale.JAPAN)
+        val calendar = Calendar.getInstance()
+        calendar.add(Calendar.MONTH,1)
+        val maxDay = calendar.getActualMaximum(Calendar.DAY_OF_MONTH)
+        for (i in 0 until maxDay) {
+            calendar.set(Calendar.DAY_OF_MONTH, i + 1)
 
-        override fun onCreate(savedInstanceState: Bundle?) {
-            super.onCreate(savedInstanceState)
-            setContentView(R.layout.activity_home)
-            val btn_show_map = findViewById<Button>(R.id.btn_show_map)
-            btn_show_map.setOnClickListener {
-                val intent = Intent(this, MainActivity::class.java)
-                startActivity(intent)
-            }
+            println(simpleDateFormat.format(calendar.time))
+            println(dayFormat.format(calendar.time))
         }
+
     }
+}

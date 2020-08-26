dc1aaafc4936f3dd26c266bdd47674797c2b953a
EiEi-stack <yccvenusstudent@gmail.com>
2020-08-26 21:06:44 +0900

    各月の日付と勤務時間計算

--- app/src/main/java/com/example/restrequestandroid/HomeActivity.kt
+++ app/src/main/java/com/example/restrequestandroid/HomeActivity.kt
@@ -21,6 +21,10 @@ class HomeActivity : AppCompatActivity() {
     lateinit var showcurrentDate: SimpleDateFormat
     lateinit var dayFormat: SimpleDateFormat
     private var maxDay = 0
+    private var totalWorkDay = 0
+    private var totalActualWorkDay = 0
+    private var totalWorkingHours = 0
+    private var totalWorkingMinutes = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
@@ -40,6 +44,7 @@ class HomeActivity : AppCompatActivity() {
                 maxDay = calendarobj.getActualMaximum(Calendar.DAY_OF_MONTH)
                 updateDateInView()
                 createTableRow()
+                setValue()
             }
         }
         tv_showDate.setOnClickListener(object : View.OnClickListener {
@@ -54,14 +59,14 @@ class HomeActivity : AppCompatActivity() {
 
             }
         })
-
-//        for (i in 0 until maxDay) {
-//            val calendar = Calendar.getInstance()
-//            calendar.set(Calendar.DAY_OF_MONTH, i + 1)
-//            println(simpleDateFormat.format(calendar.time))
-//            println(dayFormat.format(calendar.time))
-//        }
         createTableRow()
+    }
+
+    private fun setValue() {
+        totalWorkDay = 0
+        totalActualWorkDay = 0
+        totalWorkingHours = 0
+        totalWorkingMinutes = 0
     }
 
     private fun addHeader() {
@@ -117,10 +122,10 @@ class HomeActivity : AppCompatActivity() {
                 TableRow.LayoutParams.MATCH_PARENT
             )
         layoutParams.setMargins(1, 1, 1, 1)
-        tbl_row_footer.layoutParams=layoutParams
+        tbl_row_footer.layoutParams = layoutParams
 
         val params = tbl_row_footer.getLayoutParams() as TableRow.LayoutParams
-        params.span=2
+        params.span = 2
 
         for (j in 0 until 6) {
             val tv_one = TextView(applicationContext)
@@ -130,22 +135,33 @@ class HomeActivity : AppCompatActivity() {
             tv_one.setBackgroundColor(Color.LTGRAY)
             if (j == 0) {
                 tv_one.setText("当月合計")
-                tv_one.layoutParams=params
+                tv_one.layoutParams = params
             }
             if (j == 1) {
-                tv_one.setText("22日")
+                var setTotalWorkDay = ""
+                setTotalWorkDay = totalWorkDay.toString() + "日"
+                tv_one.setText(setTotalWorkDay)
             }
             if (j == 2) {
-                tv_one.setText("21日")
+                var setTotalActualWorkDay = ""
+                setTotalActualWorkDay = totalActualWorkDay.toString() + "日"
+                tv_one.setText(setTotalActualWorkDay)
             }
             if (j == 3) {
                 tv_one.setText("1日")
             }
             if (j == 4) {
-                tv_one.setText("173:40")
-            }
-            if (j == 5) {
-                tv_one.setText("実働時間")
+                var hour = 0
+                var minutes = 0
+                var totalhour = 0
+                var totalminutes = 0
+                var totaltime = ""
+                hour = totalWorkingMinutes / 60
+                minutes = totalWorkingMinutes % 60
+                totalhour = totalWorkingHours + hour
+                totalminutes = minutes
+                totaltime = totalhour.toString() + ":" + totalminutes.toString()
+                tv_one.setText(totaltime)
             }
             tbl_row_footer.addView(tv_one)
         }
@@ -163,6 +179,7 @@ class HomeActivity : AppCompatActivity() {
         addHeader()
 
         for (i in 0 until maxDay) {
+            totalWorkDay += 1
             val showCalendarDate = Calendar.getInstance()
             showCalendarDate.set(Calendar.DAY_OF_MONTH, i + 1)
             val tbl_row = TableRow(applicationContext)
@@ -174,28 +191,35 @@ class HomeActivity : AppCompatActivity() {
                 )
             layoutParams.setMargins(1, 1, 1, 1)
             for (j in 0 until 8) {
+
                 val tv_one = TextView(applicationContext)
                 tv_one.setPadding(0, 10, 0, 10)
                 tv_one.gravity = Gravity.CENTER
                 tv_one.setTextSize(12F)
-               val id_i= i.toString()
-                val id_j= j.toString()
-                val id= id_i+id_j
-                tv_one.id=id.toInt()
+                val id_i = i.toString()
+                val id_j = j.toString()
+                val id = id_i + id_j
+                tv_one.id = id.toInt()
                 if (j == 0) {
                     tv_one.setText(simpleDateFormat.format(showCalendarDate.time))
 
 
                 } else if (j == 1) {
                     tv_one.setText(dayFormat.format(showCalendarDate.time))
-                }else if (j == 2) {
+                } else if (j == 2) {
+                    totalActualWorkDay += 1
                     tv_one.setText("09:00")
                 } else if (j == 3) {
                     tv_one.setText("18:00")
-                }else if (j == 4) {
+                } else if (j == 4) {
                     tv_one.setText("01:00")
-                }else if (j == 5) {
-                    tv_one.setText("08:00")
+                } else if (j == 5) {
+                    tv_one.setText("8:30")
+                    val ab = tv_one.text
+                    val arr = ab.split(":")
+                    totalWorkingHours += arr[0].toInt()
+                    totalWorkingMinutes += arr[1].toInt()
+
                 } else if (j == 7) {
                     tv_one.setText("入力")
                 } else {
@@ -208,11 +232,17 @@ class HomeActivity : AppCompatActivity() {
 
                 if (tv_one.text == "土") {
                     tv_one.setTextColor(Color.BLUE)
+                    totalWorkDay -= 1
                 } else if (tv_one.text == "日") {
                     tv_one.setTextColor(Color.RED)
+                    totalWorkDay -= 1
                 }
                 tv_one.setOnClickListener {
-                    Toast.makeText(applicationContext, "Hey Hello"+tv_one.id, Toast.LENGTH_LONG).show()
+                    Toast.makeText(
+                        applicationContext,
+                        "Hey Hello" + totalWorkingHours,
+                        Toast.LENGTH_LONG
+                    ).show()
                 }
             }
             tbl_layout.addView(tbl_row)
